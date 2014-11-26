c040593fb15c9025e8c67e87a1de4a1cfa904c16
Mikhail Cheshkov <muuuuuu@mail.ru>
2014-11-26 17:50:29 +0600

    Fix CMake lint for out of source build

--- cmake/lint.cmake
+++ cmake/lint.cmake
@@ -1,4 +1,3 @@
-
 set(CMAKE_SOURCE_DIR ..)
 set(LINT_COMMAND ${CMAKE_SOURCE_DIR}/scripts/cpp_lint.py)
 set(SRC_FILE_EXTENSIONS h hpp hu c cpp cu cc)
@@ -10,14 +9,14 @@ cmake_policy(SET CMP0009 NEW)  # supress cmake warning
 # find all files of interest
 foreach(ext ${SRC_FILE_EXTENSIONS})
     foreach(dir ${LINT_DIRS})
-        file(GLOB_RECURSE FOUND_FILES ${CMAKE_SOURCE_DIR}/${dir}/*.${ext})
+        file(GLOB_RECURSE FOUND_FILES ${PROJECT_ROOT_DIR}/${dir}/*.${ext})
         set(LINT_SOURCES ${LINT_SOURCES} ${FOUND_FILES})
     endforeach()
 endforeach()
 
 # find all files that should be excluded
 foreach(ext ${EXCLUDE_FILE_EXTENSTIONS})
-    file(GLOB_RECURSE FOUND_FILES ${CMAKE_SOURCE_DIR}/*.${ext})
+    file(GLOB_RECURSE FOUND_FILES ${PROJECT_ROOT_DIR}/*.${ext})
     set(EXCLUDED_FILES ${EXCLUDED_FILES} ${FOUND_FILES})
 endforeach()
 
