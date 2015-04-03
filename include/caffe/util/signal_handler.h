f37bbfb84663220eb77f53c31ab96a5dd3156b1c
J Yegerlehner <jyegerlehner@yahoo.com>
2015-04-03 17:01:55 -0500

    Placate lint.

--- include/caffe/util/signal_handler.h
+++ include/caffe/util/signal_handler.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef INCLUDE_CAFFE_UTIL_SIGNAL_HANDLER_H_
+#define INCLUDE_CAFFE_UTIL_SIGNAL_HANDLER_H_
+
 #include "caffe/proto/caffe.pb.h"
 #include "caffe/solver.hpp"
 
@@ -6,16 +8,18 @@ namespace caffe {
 
 class SignalHandler
 {
-public:
+ public:
   // Contructor. Specify what action to take when a signal is received.
   SignalHandler(SolverParameter_Action SIGINT_action,
-                SolverParameter_Action SIGHUP_action );
+                SolverParameter_Action SIGHUP_action);
   ActionCallback GetActionFunction();
-private:
-  SignalHandler(); // Not implemented.
+ private:
+  SignalHandler();  // Not implemented.
   SolverParameter_Action CheckForSignals() const;
   SolverParameter_Action SIGINT_action_;
   SolverParameter_Action SIGHUP_action_;
 };
 
-} // namespace caffe
+}  // namespace caffe
+
+#endif  // INCLUDE_CAFFE_UTIL_SIGNAL_HANDLER_H_
